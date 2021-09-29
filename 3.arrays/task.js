8eb3225947c8a153ed7de386a5e1d39c7a811b54
Alexey Kurenkov <kurenkov.alexey89@yandex.ru>
2021-09-29 21:12:56 +0500

    homework3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,7 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  result = (arr1, arr2).every((item) => arr1[item] === arr2[item]);
 
   return result; // boolean
 }
@@ -10,6 +11,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+  
 
   return resultArr; // array
 }
