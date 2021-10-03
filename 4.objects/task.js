1eabaedd330682216e26e3796af62d74aeea20ad
Alexey Kurenkov <kurenkov.alexey89@yandex.ru>
2021-10-03 20:20:50 +0500

    homework4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
     // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) { 
+  if(this.marks === undefined){ 
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+  let sum = 0;
+  this.marks.reduce((acc, item) => {
+    sum = sum + item;
+    return sum;
+  }, 0);
+  this.average = (sum / this.marks.length).toFixed(2);
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  if (this.excluded !== undefined) {
+    delete this.subject;
+    delete this.marks;
+  }
+}
