2b0846a7ff928adee8d27c7e5e688fc4ec3ec7ed
Alexey Kurenkov <kurenkov.alexey89@yandex.ru>
2021-10-15 21:47:24 +0500

    homework5

---
+++ 5.classes/task.js
@@ -0,0 +1,145 @@
+class PrintEditionItem {
+  
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+  
+  fix() {
+    return this.state = this.state * 1.5;
+  }
+    
+  set state(fix) {
+    if (fix < 0) {
+      this._state = 0;
+    } else if (fix > 100) {
+      this._state = 100;
+    } else {
+      this._state = fix;
+    }
+  }
+  
+  get state() {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  
+  constructor (name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  
+  constructor (author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+
+class DetectiveBook  extends Book {
+  
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
+
+class Library {
+  
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+    console.log(this.books);
+  }
+  
+  addBook(book) {
+    if (book.state > 30) {
+      return this.books.push(book);
+    }
+  }
+  
+  findBookBy(type, value) {
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i][type] === value) {
+        return this.books[i].name;
+      }
+      return null;
+    }
+  }
+  
+  giveBookByName(bookName) {
+    for (let i = 0; i < this.books.length; i++) {
+      if (bookName === this.books[i].name) {
+        this.books.splice(i, 1);
+        return this.books[i];
+      } 
+//      return null;
+      
+      
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
